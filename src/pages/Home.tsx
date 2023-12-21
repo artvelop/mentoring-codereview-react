d346544664a8c46c995b90895d446ca5eba84ffa
artvelop <richcode99@gmail.com>
2023-12-21 22:42:30 +0900

    fix: navigate 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
-  const navigate = useNavigate();
-
-  const handleClickMove = () => {
-    navigate('/hello');
-  };
-
   return (
     <div>
       <div>Home</div>
-      <button onClick={handleClickMove}>HELLO</button>
     </div>
   );
 };
