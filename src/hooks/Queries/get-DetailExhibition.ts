6af71a4bf6c0697d2008db52cb3e0d195a97027a
taeginote <ab315601@naver.com>
2024-01-24 19:53:45 +0900

    feat : detail api 추가

---
+++ src/hooks/Queries/get-DetailExhibition.ts
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { getDetailExhibition } from '@src/apis';
+import { ExhibitionType } from './get-ExhibitionList';
+
+const getDetailExhibitionData = async (id: number) => {
+  const res = await getDetailExhibition({ id });
+  return res;
+};
+
+const useGetDetailExhibitionData = (id: number) => {
+  const { data, isLoading, refetch } = useQuery<ExhibitionType | any>(
+    ['useGetDetailExhibitionData'],
+    () => getDetailExhibitionData(id),
+  );
+
+  return { data, isLoading, refetch };
+};
+
+export default useGetDetailExhibitionData;
