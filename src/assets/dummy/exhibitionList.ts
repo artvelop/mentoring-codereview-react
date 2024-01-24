86a8899c529ec1a505e8c93e2e2a3b8da7805301
taeginote <ab315601@naver.com>
2024-01-24 17:14:55 +0900

    init : Project 생성

---
+++ src/assets/dummy/exhibitionList.ts
@@ -0,0 +1,90 @@
+export const dummyExhibitionList = [
+  {
+    id: 1,
+    title: '도톤보리 : teleport in osaka',
+    imageUrl: 'https://artvelop.s3.ap-northeast-2.amazonaws.com/code-review/light/1.jpeg',
+    place: '국립 현대 미술관',
+    price: 13500,
+    date: {
+      started: '2023.08.17',
+      ended: '2024.02.12',
+    },
+  },
+  {
+    id: 2,
+    title: '기묘한 사진전 X 청소년 이야기',
+    imageUrl: 'https://artvelop.s3.ap-northeast-2.amazonaws.com/code-review/light/2.jpeg',
+    place: '페이스 갤러리/서울',
+    price: 8500,
+    date: {
+      started: '2024.01.03',
+      ended: '2024.04.17',
+    },
+  },
+  {
+    id: 3,
+    title: '도리도리 : 버킷리스트',
+    imageUrl: 'https://artvelop.s3.ap-northeast-2.amazonaws.com/code-review/light/3.jpeg',
+    place: '하우스 서울/서울',
+    price: 8500,
+    date: {
+      started: '2024.01.09',
+      ended: '2025.01.03',
+    },
+  },
+  {
+    id: 4,
+    title: '그 해 우리는 : 달빛축제',
+    imageUrl: 'https://artvelop.s3.ap-northeast-2.amazonaws.com/code-review/light/4.jpeg',
+    place: '하우스 서울/서울',
+    price: 5000,
+    date: {
+      started: '2024.01.09',
+      ended: '2024.08.15',
+    },
+  },
+  {
+    id: 5,
+    title: '차가운 도시 : Cold City',
+    imageUrl: 'https://artvelop.s3.ap-northeast-2.amazonaws.com/code-review/light/5.jpeg',
+    place: '라이즈아트/서울',
+    price: 6500,
+    date: {
+      started: '2024.02.09',
+      ended: '2024.07.11',
+    },
+  },
+  {
+    id: 6,
+    title: '어두운 성전 X 해리포터 마법 상점',
+    imageUrl: 'https://artvelop.s3.ap-northeast-2.amazonaws.com/code-review/light/6.jpeg',
+    place: '스페이스K/서울',
+    price: 7500,
+    date: {
+      started: '2024.01.09',
+      ended: '2024.06.05',
+    },
+  },
+  {
+    id: 7,
+    title: '울지마요 그대',
+    imageUrl: 'https://artvelop.s3.ap-northeast-2.amazonaws.com/code-review/light/7.jpeg',
+    place: '잠실 롯데타워',
+    price: 12500,
+    date: {
+      started: '2023.08.11',
+      ended: '2024.02.01',
+    },
+  },
+  {
+    id: 8,
+    title: '바이닐 그 시절',
+    imageUrl: 'https://artvelop.s3.ap-northeast-2.amazonaws.com/code-review/light/8.jpeg',
+    place: '하우스 성수',
+    price: 4500,
+    date: {
+      started: '2023.07.11',
+      ended: '2024.03.15',
+    },
+  },
+];
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from './Router';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './Styles/global';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Router />
+    <GlobalStyles />
   </React.StrictMode>,
 );
 
