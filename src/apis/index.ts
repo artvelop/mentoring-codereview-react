0b0eeec1ad03ca9922e3d0c97ebb8ed62f4ec37f
cham0287 <crtmt97@gmail.com>
2024-01-25 00:31:36 +0900

    Feat: 예매 페이지 상세 데이터 불러오기

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -12,7 +12,7 @@ export const getApiExhibitionList = async (): Promise<Exhibition[]> => {
 };
 
 // 전시회 상세 불러오기 API
-export const getDetailExhibition = async ({ id }: { id: number }) => {
+export const getDetailExhibition = async ({ id }: { id: number }): Promise<Exhibition | Error> => {
   const response = dummyExhibitionList.find((item) => item.id === id);
 
   await sleep({ ms: 500 });
@@ -21,5 +21,5 @@ export const getDetailExhibition = async ({ id }: { id: number }) => {
     return new Error('존재하지 않는 전시회 입니다');
   }
 
-  return response;
+  return response as Exhibition;
 };
--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { getDetailExhibition } from '@src/apis';
+import { useParams } from 'react-router-dom';
 
 const Book = () => {
+  const { id } = useParams();
+
+  const { data: exhibitionDetail } = useQuery({
+    queryKey: ['exhibitionDetail', id],
+    queryFn: () => getDetailExhibition({ id: Number(id) }),
+  });
+  console.log(exhibitionDetail);
+
   return <div>예매페이지</div>;
 };
 
