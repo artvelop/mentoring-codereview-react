d323074fa79cf812d2d5b217120932f229d5c2d1
taeginote <ab315601@naver.com>
2024-01-24 17:12:01 +0900

    sytle : button 컴포넌트 생성

---
+++ src/components/Button/Button.style.ts
@@ -0,0 +1,46 @@
+import styled, { css } from 'styled-components';
+
+type Props = {
+  variant: 'default-black' | 'orange' | 'gray';
+  size: 'small' | 'default';
+  fontSize: 'default';
+  // disabled: boolean
+};
+
+const variantCSS = {
+  'default-black': css`
+    background-color: black;
+  `,
+  'orange': css`
+    background-color: orange;
+  `,
+  'gray': css`
+    background-color: gray;
+  `,
+};
+
+const sizeCSS = {
+  default: css`
+    width: 100%;
+    height: 4.8rem;
+  `,
+  small: css`
+    width: 2.6rem;
+    height: 1.2rem;
+  `,
+};
+
+const fontSizeCSS = {
+  default: css`
+    font-size: 8px;
+  `,
+};
+
+export const Button = styled.button<Props>`
+  color: white;
+  ${({ variant }) => variantCSS[variant]}
+  ${({ size }) => sizeCSS[size]}
+	${({ fontSize }) => fontSizeCSS[fontSize]}
+	cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
+  border-radius: 5px;
+`;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,37 @@
+import * as S from './Button.style';
+
+type OnClickType = (event: React.MouseEvent<HTMLButtonElement>) => void;
+
+interface ButtonProps {
+  variant?: 'default-black' | 'orange' | 'gray';
+  size?: 'small' | 'default';
+  fontSize?: 'default';
+  type?: 'submit' | 'button';
+  disabled?: boolean;
+  children?: React.ReactNode;
+  onClick?: OnClickType;
+}
+
+function Button(props: ButtonProps) {
+  const {
+    variant = 'default-black',
+    size = 'default',
+    fontSize = 'default',
+    children,
+    ...rest
+  } = props;
+
+  return (
+    <S.Button
+      variant={variant}
+      size={size}
+      fontSize={fontSize}
+      // disabled={!!rest.disabled}
+      {...rest}
+      title="Button"
+    >
+      {children}
+    </S.Button>
+  );
+}
+export default Button;
