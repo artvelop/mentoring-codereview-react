8b17bcb1683fb0e3616393e72a2826ce610e4aaf
cham0287 <crtmt97@gmail.com>
2024-01-24 23:59:02 +0900

    Feat: 찜목록 없을 시 UI 처리

--- src/pages/Picked.tsx
+++ src/pages/Picked.tsx
@@ -5,18 +5,27 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const Picked = () => {
-  const { data: exhibitionList, isLoading } = useQuery<Exhibition[]>({
+  const { data: exhibitionList } = useQuery<Exhibition[]>({
     queryKey: ['exhibitionList'],
   });
   const { pickedExhibitionIDs } = usePickedExhibitionStore();
 
+  const pickedExhibitionList = exhibitionList?.filter((exhibition) =>
+    pickedExhibitionIDs.includes(exhibition.id),
+  );
+
   return (
     <div>
-      {isLoading && <div>Loading...</div>}
-      {exhibitionList &&
-        exhibitionList
-          .filter((exhibition) => pickedExhibitionIDs.includes(exhibition.id))
-          .map((exhibition) => <ExhibitionCard key={exhibition.id} exhibition={exhibition} />)}
+      {pickedExhibitionList && pickedExhibitionList.length > 0 ? (
+        pickedExhibitionList.map((exhibition) => (
+          <ExhibitionCard key={exhibition.id} exhibition={exhibition} />
+        ))
+      ) : (
+        <div className="h-[100dvh] flex flex-col items-center justify-center">
+          <p>찜해놓은 전시회가 없습니다</p>
+          <p className="text-gray-400">맘에 드는 전시회가 있다면 찜해보세요</p>
+        </div>
+      )}
     </div>
   );
 };
