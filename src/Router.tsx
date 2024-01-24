570218aa78b80c33052466bed296d722122e1205
cham0287 <crtmt97@gmail.com>
2024-01-24 09:12:14 +0900

    Feat: Picked 라우트 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Home } from '@pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProvidersWrapper from '@components/ProvidersWrapper';
+import Picked from '@pages/Picked';
 
 function Router() {
   return (
@@ -9,6 +10,9 @@ function Router() {
       <ProvidersWrapper>
         <Routes>
           <Route path="/" element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path="/picked" element={<Picked />} />
         </Routes>
       </ProvidersWrapper>
     </BrowserRouter>
---
+++ src/pages/Picked.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Picked = () => {
+  return <div></div>;
+};
+
+export default Picked;
