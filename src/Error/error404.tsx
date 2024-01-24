7efa0cc72f1400e2c14810eaab9348c44194b717
taeginote <ab315601@naver.com>
2024-01-24 21:25:47 +0900

    feat : 404Error 처리

---
+++ src/Error/error404.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+function Error404() {
+  return (
+    <S.Wrapper>
+      <h1>
+        <span>404</span> Error Page
+      </h1>
+    </S.Wrapper>
+  );
+}
+export default Error404;
+
+const Wrapper = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  & > h1 {
+    & > span {
+      color: red;
+    }
+  }
+`;
+
+const S = { Wrapper };
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Home } from '@pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Error404 from './Error/error404';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
