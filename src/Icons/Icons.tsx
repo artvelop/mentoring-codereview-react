a206750a5353f4f23e2b78ac2ac9846a73efe5e7
taeginote <ab315601@naver.com>
2024-01-25 23:02:22 +0900

    feat : icons 추가

--- src/Icons/Icons.tsx
+++ src/Icons/Icons.tsx
@@ -1,19 +1,24 @@
-import { FaRegStar } from 'react-icons/fa6';
-import { FaStar } from 'react-icons/fa6';
+import { FaRegStar, FaStar } from 'react-icons/fa6';
 import { IoTicketSharp } from 'react-icons/io5';
+import { RiArrowGoBackLine } from 'react-icons/ri';
 
 interface OnClickIconType {
+  size?: string;
   onClick?: React.MouseEventHandler<SVGSVGElement>;
 }
 
-export const NotFillStarIcon = ({ onClick }: OnClickIconType) => (
-  <FaRegStar size={'25'} onClick={onClick} color="#FB9B00" />
+export const NotFillStarIcon = ({ size, onClick }: OnClickIconType) => (
+  <FaRegStar size={size} onClick={onClick} color="#FB9B00" />
 ); // 별빈거
 
-export const FillStarIcon = ({ onClick }: OnClickIconType) => (
-  <FaStar size={'25'} onClick={onClick} color="#FB9B00" />
+export const FillStarIcon = ({ size, onClick }: OnClickIconType) => (
+  <FaStar size={size} onClick={onClick} color="#FB9B00" />
 ); // 별채운거
 
 export const TicketIcon = ({ onClick }: OnClickIconType) => (
   <IoTicketSharp size={'25'} onClick={onClick} color="#FB9B00" />
-); // 별채운거
+); // 티켓
+
+export const BackIcon = ({ onClick }: OnClickIconType) => (
+  <RiArrowGoBackLine size={'30'} onClick={onClick} color="#FB9B00" />
+); // 뒤로가기
